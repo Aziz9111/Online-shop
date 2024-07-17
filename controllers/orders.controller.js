c63bd401e8e7d74e06f092833e51dd42c1529db2
Aziz Ahmed <abdulazizbinafif11@gmail.com>
2024-07-17 18:08:27 +0300

    Adding strip for payment

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,5 +1,8 @@
 const Order = require("../models/orders.model");
 const User = require("../models/user.model");
+const stripe = require("stripe")(
+  "sk_test_51PdYbbETblsGBR2cm0vVXBRYzlDVx3JD6i0cmAYSjPhzB1FKnQruVC7PVTtTpSG5UFa4QjeOmg288yoQoi1hcyOC00steQoSX1"
+);
 
 async function getOrders(req, res) {
   try {
@@ -33,10 +36,38 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect("/orders");
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.item.map((item) => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.product.title,
+          },
+          unit_amount: +item.product.price,
+        },
+        quantity: item.quantity,
+      };
+    }),
+    mode: "payment",
+    success_url: "http://localhost:3000/orders/success",
+    cancel_url: "http://localhost:3000/orders/fail",
+  });
+
+  res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+  res.render("customers/orders/success");
+}
+
+function getFail(req, res) {
+  res.render("customers/orders/fail");
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFail: getFail,
 };
